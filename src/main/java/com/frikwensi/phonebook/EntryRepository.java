a80a63d540af72bded8c99995f415be22af85473
Nhlanhla <nhlx2@frikwensi.com>
2020-03-09 04:49:20 +0200

    Removed UI hosting and enabled CORS

--- src/main/java/com/frikwensi/phonebook/EntryRepository.java
+++ src/main/java/com/frikwensi/phonebook/EntryRepository.java
@@ -1,7 +1,11 @@
 package com.frikwensi.phonebook;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@CrossOrigin
+@RepositoryRestResource
 public interface EntryRepository extends CrudRepository<Entry, Long> {
 
 }
