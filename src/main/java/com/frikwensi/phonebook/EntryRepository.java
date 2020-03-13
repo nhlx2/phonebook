c615a1ec4e5f307d0739ded880844d0bd5c760df
Nhlanhla <nhlx2@frikwensi.com>
2020-03-13 19:31:17 +0200

    Enabled search by name facility

--- src/main/java/com/frikwensi/phonebook/EntryRepository.java
+++ src/main/java/com/frikwensi/phonebook/EntryRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @CrossOrigin
 @RepositoryRestResource
 public interface EntryRepository extends CrudRepository<Entry, Long> {
-
+    List<Entry> findByNameContaining(String name);
 }
